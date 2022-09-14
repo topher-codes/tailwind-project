96e12f32a9b0e0e9a4ab47538da1a62bd2f0ee56
Chris <ghost@drednaut.net>
2022-09-14 14:43:17 -0700

    Added example tailwind component
    Signed-off-by: Chris <ghost@drednaut.net>

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Example from './components/example';
 
 function App() {
 	return (
 		<div className="App">
 			<header className="App-header">
 				<p className="text-6xl border-l-fu">Welcome</p>
+				<Example />
 			</header>
 		</div>
 	);
